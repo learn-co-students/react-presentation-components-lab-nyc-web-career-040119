4ba7de29e58892679946d0949094c7e9aedc609b
Jeffrey Marks <JKDMarks@gmail.com>
2019-06-07 13:38:42 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
+function changeMood() {
+  console.log("clicked")
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={changeMood} />
   </div>,
   document.getElementById('root')
 );
